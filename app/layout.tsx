da79d053a5898e8634eacb49ec7c65cec3404495
mohammedalasly <mohammedalasly5@gmail.com>
2025-08-15 21:13:48 +0300

    Implement Clerk Auth & Billing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Bricolage_Grotesque } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
+import "./globals.css"
 import Navbar from "@/components/Navbar"
 
 const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
-});
+	variable: "--font-bricolage",
+	subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
 	title: "Trenavo",
@@ -14,16 +15,18 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
-        </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${bricolage.variable} antialiased`}>
+				<ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+					<Navbar />
+					{children}
+				</ClerkProvider>
+			</body>
+		</html>
+	)
 }
