c84989858612c868d14d05a75649b956cf7cb53c
mohammedalasly <mohammedalasly5@gmail.com>
2025-08-22 19:45:01 +0300

    update the pricing/subscription page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -4,14 +4,12 @@ import HeroSection from "@/components/HeroSection"
 const Subscription = () => {
 	return (
 		<>
-			<HeroSection
-				title="Pricing Plans and Subscriptions"
-			/>
-			<main className="min-h-screen pt-10 pb-20 px-2">
-				<div className="flex justify-center items-center ">
+			<HeroSection title="Pricing Plans and Subscriptions" />
+			<section className="max-w-7xl mx-auto py-20 px-2">
+				<div className="flex justify-center items-center">
 					<PricingTable />
 				</div>
-			</main>
+			</section>
 		</>
 	)
 }
